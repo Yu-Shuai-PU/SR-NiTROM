Fixed the OOM bug by reformulating the gradient expression to avoid large matrix calculation

Examples/LNS/classes.py
@@ -380,7 +380,7 @@ class optimization_objects:
                 'forcing_interp':   a scipy interpolator f that gives us a forcing f(t)
         """
         z = zc[:-1]
-
+        # print(f"evaluate_rom_rhs called at time t = {t}, z_magnitude = {np.linalg.norm(z)}")
         if np.linalg.norm(z) >= self.state_mag_threshold:
             dzdt = 0.0*z
             dcdt = 0.0

Examples/LNS/nitrom_functions.py
@@ -133,9 +133,6 @@ def create_objective_and_gradient(manifold,opt_obj,mpi_pool,fom):
         # Gauss-Legendre quadrature points and weights
         tlg, wlg = np.polynomial.legendre.leggauss(opt_obj.leggauss_deg)
         wlg = np.asarray(wlg)
-
-        # Define operators derived from bases
-        Proj_perp = np.eye(Phi.shape[0]) - Psi@(PhiF.T)
         
         cdot_denom_linear = np.zeros(r)
         udx_linear = Psi.T @ PhiF_dx
@@ -197,7 +194,7 @@ def create_objective_and_gradient(manifold,opt_obj,mpi_pool,fom):
                 grad_Phi -= 2 * weight_X_j * np.einsum('i,j', e_X_fitted_weighted_j, z_j)
                 grad_Phi -= 2 * weight_cdot_j * e_cdot_j * cdot_j * u0dxx_zj_outer_product / cdot_denom_j
                 grad_Psi += 2 * weight_X_j * np.einsum('i,j',PhiF@z_j, PhiF.T@e_X_fitted_weighted_j)
-                grad_Psi += 2 * weight_cdot_j * e_cdot_j * cdot_j * (PhiF @ u0dxx_zj_outer_product.T @ PhiF) / cdot_denom_j
+                grad_Psi += 2 * weight_cdot_j * e_cdot_j * cdot_j * PhiF @ (u0dxx_zj_outer_product.T @ PhiF) / cdot_denom_j ### Warning: check this term carefully!!! Heavy memory usage!!!
                 
                 for (count,p) in enumerate(opt_obj.poly_comp):
                     equation = ','.join(ascii[:p])
@@ -271,8 +268,8 @@ def create_objective_and_gradient(manifold,opt_obj,mpi_pool,fom):
                     grad_Phi += Psi_dx @ Int_cdot_xi_z_j - Int_eta_cdot_u0dxx_outer_z_j_cdot_denom
 
                     grad_Psi -= PhiF_dx @ Int_cdot_xi_z_j.T
-                    grad_Psi += PhiF @ (Int_cdot_xi_z_j.T @ Psi.T) @ PhiF_dx
-                    grad_Psi += PhiF @ Int_eta_cdot_u0dxx_outer_z_j_cdot_denom.T @ PhiF
+                    grad_Psi += PhiF @ Int_cdot_xi_z_j.T @ (Psi.T @ PhiF_dx)
+                    grad_Psi += PhiF @ (Int_eta_cdot_u0dxx_outer_z_j_cdot_denom.T @ PhiF)
 
                     for (count,p) in enumerate(opt_obj.poly_comp):
                         equation = ','.join(ascii[:p+1])
@@ -302,10 +299,11 @@ def create_objective_and_gradient(manifold,opt_obj,mpi_pool,fom):
             grad_Phi -= 2 * weight_X_0 * np.einsum('i,j', e_X_fitted_weighted_0, z_0)
             grad_Phi -= 2 * weight_cdot_0 * e_cdot_0 * cdot_0 * u0dxx_z_0_outer_0 / cdot_denom_0
             grad_Psi += 2 * weight_X_0 * np.einsum('i,j',PhiF@z_0, PhiF.T@e_X_fitted_weighted_0)
-            grad_Psi += 2 * weight_cdot_0 * e_cdot_0 * cdot_0 * (PhiF @ u0dxx_z_0_outer_0.T @ PhiF) / cdot_denom_0
+            grad_Psi += 2 * weight_cdot_0 * e_cdot_0 * cdot_0 * PhiF @ (u0dxx_z_0_outer_0.T @ PhiF) / cdot_denom_0
             grad_Psi -= np.einsum('i,j', opt_obj.X_fitted_weighted[k,:,0], xi0_j)
             
-            grad_Phi = (Proj_perp @ grad_Phi) @ F.T
+            # Project gradients onto the tangent space            
+            grad_Phi = (grad_Phi - Psi @ (PhiF.T @ grad_Phi)) @ F.T
         
         # Compute the gradient of the stability-promoting term
         if opt_obj.l2_pen != None and mpi_pool.rank == 0:

Examples/LNS/train_SR_NiTROM.py
@@ -90,13 +90,13 @@ fname_time = traj_path + "time.npy"
 #%% # Generate and save trajectory
 n_traj = 1
 
-Lx = 48
+Lx = 32
 Ly = 2 # from -1 to 1
-Lz = 24
+Lz = 16
 
-nx = 96
-ny = 65 # ny includes the boundary points when using Chebyshev grid
-nz = 96
+nx = 32
+ny = 33 # ny includes the boundary points when using Chebyshev grid
+nz = 32
 
 x = np.linspace(0, Lx, num=nx, endpoint=False)
 y = np.cos(np.pi * np.linspace(0, ny - 1, num=ny) / (ny - 1))  # Chebyshev grid in y direction, location from 1 to -1
@@ -144,15 +144,15 @@ pool.load_data()
 
 T_final = pool.time[-1]
 
-r = 40 # ROM dimension, should account for 99.5% energy
+r = 40 # ROM dimension, should account for 99.5% energy, according to our test, 40 is fine for a single benchmark trajectory.
 
 initialization = "POD-Galerkin" # "POD-Galerkin" or "Previous NiTROM"
 # initialization = "Previous NiTROM"
 NiTROM_coeff_version = "new" # "old" or "new" for loading the NiTROM coefficients before or after the latest training
 # NiTROM_coeff_version = "old"
-# training_objects = "tensors_and_bases"
+training_objects = "tensors_and_bases"
 # training_objects = "tensors" 
-training_objects = "no_alternating"
+# training_objects = "no_alternating"
 manifold = "Grassmann" # "Grassmann" or "Stiefel" for Psi manifold
 # manifold = "Stiefel"
 weight_decay_rate = 1 # if weight_decay_rate is not 1, then the snapshots at larger times will be given less weights in the cost function
@@ -163,7 +163,7 @@ initial_relative_weight_cdot = 0.05
 final_relative_weight_cdot = 0.05
 sigmoid_steepness_cdot_weight = 2.0
 k0 = 0
-kouter = 1
+kouter = 5
 kinner_basis = 3
 kinner_tensor = 3
 initial_step_size_basis = 1e-1
@@ -467,6 +467,7 @@ opt_obj_kwargs = {
 'weight_decay_rate': weight_decay_rate
 }
 opt_obj = classes.optimization_objects(*opt_obj_inputs,**opt_obj_kwargs)
+opt_obj.recalibrate_weights(weight_traj, weight_shifting_amount, weight_shifting_speed)
 cost, grad, hess = nitrom_functions.create_objective_and_gradient(M,opt_obj,pool,fom)
 problem = pymanopt.Problem(M,cost,euclidean_gradient=grad)
 check_gradient(problem,x=point)
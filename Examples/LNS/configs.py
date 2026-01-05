0bd76c1cc51a9b1dae5aae20e861218483c4a039
Yu Shuai <yu_shuai@princeton.edu>
2026-01-04 23:26:54 -0500

    Add comparising parameters from Henningson 1993

--- Examples/LNS/configs.py
+++ Examples/LNS/configs.py
@@ -16,17 +16,25 @@ class SimConfigs:
     nx: int = 64
     ny: int = 33
     nz: int = 16
-    Re: float = 1200
-    T: float = 200
-    dt: float = 0.5
-    nsave: int = 2
+    Re: float = 2000
+    # T: float = 200
+    # Lx: float = 48
+    # Lz: float = 24
+    # nx: int = 96
+    # ny: int = 65
+    # nz: int = 96
+    # Re: float = 3000
+    T: float = 500
+    dt: float = 1
+    nsave: int = 1
     amp: float = 1.0
     small_amp: float = 0.0001
     medium_amp: float = 0.0699
     large_amp: float = 0.1398
     traj_path: str = "./trajectories/"
     data_path: str = "./data/"
-    fig_path: str = "./figures/"
+    fig_path_SRG: str = "./figures/SRG/"
+    fig_path_SRN: str = "./figures/SRN/"
     
     # Training methods
     
@@ -34,23 +42,25 @@ class SimConfigs:
     r : int = 20 # dimension of the ROM
     poly_comp: List[int] = field(default_factory=lambda: [1])
     initialization: str = "POD-Galerkin" # "POD-Galerkin" or "Previous NiTROM"
+    # initialization: str = "Previous NiTROM" # "POD-Galerkin" or "Previous NiTROM"
     NiTROM_coeff_version: str = "new" # "old" or "new" for loading the NiTROM coefficients before or after the latest training
     training_objects: str = "tensors_and_bases" # "tensors_and_bases" or "tensors" or "no_alternating"
-    manifold: str = "Grassmann" # "Grassmann" or "Stiefel" for Psi manifold choice
+    # training_objects: str = "no_alternating" # "tensors_and_bases" or "tensors" or "no_alternating"
+    manifold: str = "Stiefel" # "Grassmann" or "Stiefel" for Psi manifold choice
     timespan_percentage_POD: float = 1.00 # percentage of the entire timespan used for POD (always use all snapshots for POD)
-    timespan_percentage_NiTROM_training: float = 0.02 # percentage of the entire timespan used for NiTROM training
+    timespan_percentage_NiTROM_training: float = 0.1 # percentage of the entire timespan used for NiTROM training
     
 
     # Parameters for relative weights between different terms in the cost function
     
     weight_decay_rate: float = 1 # if weight_decay_rate is not 1, then the snapshots at larger times will be given less weights in the cost function
-    # initial_relative_weight_c: float = 0.5 # as the training iterations go on, we will gradually increase the weight of shift amount term in the cost function
-    # final_relative_weight_c: float = 0.5  # the final relative weight
+    initial_relative_weight_c: float = 100 # as the training iterations go on, we will gradually increase the weight of shift amount term in the cost function
+    final_relative_weight_c: float = 100  # the final relative weight
     initial_relative_weight_c: float = 0.0
     final_relative_weight_c: float = 0.0
     sigmoid_steepness_c_weight: float = 2.0
-    # initial_relative_weight_cdot: float = 0.05
-    # final_relative_weight_cdot: float = 0.05
+    initial_relative_weight_cdot: float = 1
+    final_relative_weight_cdot: float = 1
     initial_relative_weight_cdot: float = 0.0
     final_relative_weight_cdot: float = 0.0
     sigmoid_steepness_cdot_weight: float = 2.0
@@ -59,11 +69,11 @@ class SimConfigs:
     
     k0: int = 0
     kouter: int = 5
-    kinner_basis: int = 3
-    kinner_tensor: int = 3
+    kinner_basis: int = 10
+    kinner_tensor: int = 5
     
-    initial_step_size_basis: float = 1e-1
-    initial_step_size_tensor: float = 1e-4
+    initial_step_size_basis: float = 1e-2
+    initial_step_size_tensor: float = 1e-5
     initial_step_size: float = 1e-3
     
     step_size_decrease_rate: float = 0.9
@@ -87,7 +97,8 @@ class SimConfigs:
         
         os.makedirs(self.traj_path, exist_ok=True)
         os.makedirs(self.data_path, exist_ok=True)
-        os.makedirs(self.fig_path, exist_ok=True)
+        os.makedirs(self.fig_path_SRG, exist_ok=True)
+        os.makedirs(self.fig_path_SRN, exist_ok=True)
                 
         self.x = np.linspace(0, self.Lx, num=self.nx, endpoint=False)
         self.y = np.cos(np.pi * np.linspace(0, self.ny - 1, num=self.ny) / (self.ny - 1))
@@ -103,7 +114,8 @@ class SimConfigs:
         self.time = self.dt * np.linspace(0, int(self.T/self.dt), int(self.T/self.dt) + 1, endpoint=True)
         self.tsave = self.time[::self.nsave]
         
-        self.t_check_list = np.linspace(0, self.T, num = int(self.T/10) + 1, endpoint=True).tolist()
+        self.t_check_list_POD = np.linspace(0, int(self.timespan_percentage_POD * self.T), num = int(self.timespan_percentage_POD * self.T/10) + 1, endpoint=True).tolist()
+        self.t_check_list_SRN = np.linspace(0, int(self.timespan_percentage_NiTROM_training * self.T), num = int(self.timespan_percentage_NiTROM_training * self.T/10) + 1, endpoint=True).tolist()
         self.y_check = -0.56
         
         self.fname_traj_template = self.data_path + "traj_template.npy"
@@ -132,6 +144,13 @@ class SimConfigs:
         self.snapshot_end_time_idx_POD = 1 + int(self.timespan_percentage_POD * (len(self.tsave) - 1)) # 1001
         self.snapshot_start_time_idx_NiTROM_training = 0
         self.snapshot_end_time_idx_NiTROM_training = 1 + int(self.timespan_percentage_NiTROM_training * (len(self.tsave) - 1))
+        
+        self.psi0 = np.zeros((self.nx, self.ny, self.nz, self.n_traj))
+
+        for idx in range(self.n_traj):
+            # self.psi0[:, :, :, idx] = self.amp * (1 - self.Y**2)**2 * (self.X - self.Lx/2)**2 * ((self.Z - self.Lz/2)/2)**2 * np.exp(-((self.X - self.Lx/2)/2)**2 - ((self.Z - self.Lz/2)/2)**2) 
+            self.psi0[:, :, :, idx] = self.amp * (1 - self.Y**2)**2 * ((self.X - self.Lx/2)/2)**2 * (self.Z - self.Lz/2)**2 * np.exp(-((self.X - self.Lx/2)/2)**2 - ((self.Z - self.Lz/2)/2)**2) 
+    
         
 def load_configs():
     return SimConfigs()
